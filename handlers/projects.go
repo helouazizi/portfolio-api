a2ea52cf12d0933f1b3b04c20048af34843a7ca7
helouazizi <ouazizi2code@gmail.com>
2025-04-07 17:06:38 +0100

    add get prijects handler

---
+++ handlers/projects.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"portfolio-api/models"
+	"portfolio-api/utils"
+)
+
+func GetProjects(w http.ResponseWriter, r *http.Request) {
+	var projects []models.Project
+	err := utils.LoadJSON("./storage/projects.json", &projects)
+	if err != nil {
+		http.Error(w, "Failed to load projects", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(projects)
+}
