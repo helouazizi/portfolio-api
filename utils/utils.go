5e2775fb6a246907aed4de813672055d0d3915c0
helouazizi <ouazizi2code@gmail.com>
2025-04-07 16:42:22 +0100

    add load json function

---
+++ utils/utils.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func LoadJSON[T any](filePath string, target *[]T) error {
+	// lets read the json file data into a slice of bytes
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to read data from : %s", filePath)
+	}
+
+	// lets unmarshel the content into our target
+	if err := json.Unmarshal(content, &target); err != nil {
+		return fmt.Errorf("failed to Unmarshal data into : %v", target)
+	}
+	return nil
+}
